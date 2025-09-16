5553d6328bc87c3ec79f6cdf5e48003359fd308f
Rogier Kroon <info@chronosolutions.nl>
2025-09-16 20:45:05 +0200

    fix(og-image): switch blog opengraph image route to nodejs runtime to allow filesystem reads

--- app/(site)/blog/[slug]/opengraph-image.tsx
+++ app/(site)/blog/[slug]/opengraph-image.tsx
@@ -1,7 +1,8 @@
 import { ImageResponse } from 'next/og'
 import { getPostBySlug } from '@/lib/blog'
 
-export const runtime = 'edge'
+// Use Node.js runtime because we read MDX files from the filesystem
+export const runtime = 'nodejs'
 export const size = { width: 1200, height: 630 }
 export const contentType = 'image/png'
 
