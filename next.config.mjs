3a01cf539999b51042d627f5e25c3de15c2472a6
Rogier Kroon <info@chronosolutions.nl>
2025-12-05 11:43:09 +0100

    chore: tighten next config for seo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compress: true,
+  poweredByHeader: false,
+  images: {
+    domains: ['www.probrandwacht.nl', 'probrandwacht.nl'],
+  },
 
   async redirects() {
     return [
